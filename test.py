cb81370904bd7a98a267ebb24966fc1252d4366a
Kent Shih <shihm@oregonstate.edu>
2016-09-23 23:16:16 -0700

    Quickselect

---
+++ test.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+import random
+a = [5,2,57,2,82,6]
+'''
+for element in a:
+	print element
+'''
+index = random.randrange(len(a))
+value = a[index]
+print "index", index
+print value
