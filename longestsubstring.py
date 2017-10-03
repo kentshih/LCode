f50b95b410e379f7c2cdb4c85f4938028a031e4e
Kent Shih <shihm@oregonstate.edu>
2017-10-02 23:36:02 -0700

    longessubstring

---
+++ longestsubstring.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        longestnum = 0
+        longstr = ""
+        tempnum = 0
+        tempstr = ""
+        keepgo = False
+        for x in s:
+            if longestnum == 0:
+                longstr = x
+                longestnum = 1
+                continue
+            if longestnum > 0:
+                if longstr[-1] == x:
+                    keepgo = True
+                    continue
+                if keepgo == True:
+                    longstr += x
+                    longestnum += 1
+                    keepgo = False
+            
+        return longestnum
+            
