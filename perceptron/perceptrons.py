bf45e7dd9e477e2c4e3834ccd68672060f7771be
Kent Shih <shihm@oregonstate.edu>
2017-10-13 01:36:39 -0700

    perceptron mira optimal

--- perceptron/perceptrons.py
+++ perceptron/perceptrons.py
@@ -2,42 +2,31 @@
 
 # import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-def check_error(w, dataset):
-    result = None
-    error = 0
-    for x, s in dataset:
-        x = np.array(x)
-        if int(np.sign(w.T.dot(x))) != s:
-            result =  x, s
-            error += 1
-    print  "error=%s/%s" % (error, len(dataset))
-    return result
 
-def pla(dataset):
-    w = np.zeros(9)
-    while check_error(w, dataset) is not None:
-        x, s = check_error(w, dataset)
-        w += s * x
-    return w
+def pla1(file,w,dictdata,avgw,iteration,mira,p):
+    f = open(file,'r')
+    smart = 1
+    bestw = w
+    bestrate = 90.0
+    besttime = 0.0
+    bestratea = 90.0
+    besttimea = 0.0
 
-def pla1(file,w,dictdata,binfea,avgw):
+    f = open(file,'r')
     error = 0
     avgerror = 0
-    f = open(file,'r')
-    x = np.zeros(261)
-    xx = np.zeros(261)
-    count = 1
-    lines = 1
-    bias = 0
-    sign = 0
-    smart = 0
+    x = np.zeros(260)
+    xx = np.zeros(260)
+    count = 0 #diction num
     avgcount = 1
-    for line in f:
+    lines = 1
+    sign = 0
+    for line in f:  ## set data
+        
         frame = line.split(", ")
-        elements = 0
-        x = np.zeros(261)
-        # print frame
+        x = np.zeros(260)
         if frame[9] == "<=50K\n":
             sign = -1
         if frame[9] == ">50K\n":
@@ -45,218 +34,184 @@ def pla1(file,w,dictdata,binfea,avgw):
         frame[0] = "Y" +frame[0]
         frame[7] = "Hr"+frame[7]
         x.fill(-1)
-        for element in frame:
+
+        for element in frame[:9]:
             if element not in dictdata:
                 dictdata[element] = count
-                binfea[element] = 0
                 count += 1
-            
-            if sign == 1:
-                binfea[element] += 1
-            else:
-                binfea[element] -= 1
-            
-            # x[elements] = dictdata[element]
-            x[dictdata[element]] = 1
-            print "x: ", dictdata[element], "= 1"
-            # if binfea[element] > 0:
-            #     x[elements] = 1
-            # if binfea[element] < 0:
-            #     x[elements] = 0
-            
-            elements += 1
-        print "---"
-        print x
-        # x[0] = dictdata[frame[0]]
-        # x[7] = dictdata[frame[7]]
-        # x[9] = 1
-        # x[9] = np.linalg.norm(x)
-        np.set_printoptions(formatter={'float': '{: 6.1f}'.format})
-        # print "x: ",x , sign
-        # print "avgw: ",avgw
-        # print "xx: ",xx
+            x[dictdata[element]] = 1    
 
-        if lines == 100:
-            avgw = w
-        
-        if int(np.sign(w.T.dot(x))) != sign: 
-            error += 1
+        if lines == 1000:            
+            avgw = w                
+
+        if w.T.dot(x) * sign <= 0:      #PLA
+            error += 1            
+            if bestrate > (float(error) / lines):                
+                bestrate = (float(error) / lines)                
+                besttime = (float(lines)/ 1000)            
             w += sign * x
-        if smart == 0:
-            if int(np.sign(avgw.T.dot(x))) != sign:
-                avgerror += 1
-            xx += w
-        if smart == 1:
-            if int(np.sign(avgw.T.dot(x))) != sign:
-                avgerror += 1
-                xx += sign * x * avgcount
+            dist = np.linalg.norm(x)
+            # print (sign - w.T.dot(x)),"/", dist*dist,"=", (sign - w.T.dot(x)) / float(dist*dist)
+            # time.sleep(1)
+            # print mira
+            # time.sleep(1)
+            if mira >= 1:
+                w += x * (sign - w.T.dot(x)) / float(dist*dist)
         
+        if w.T.dot(x) * sign > 0 and mira == 2 and w.T.dot(x) * sign <= p:
+            w += x * (sign - w.T.dot(x)) / float(dist*dist)
+             
+        if smart == 0:    #naive avg
+            if avgw.T.dot(x) * sign <= 0:                
+                avgerror += 1
+                avgw += sign * x                
+                if bestratea > (float(avgerror) / lines):                    
+                    bestratea = (float(avgerror) / lines)                    
+                    besttimea = (float(lines)/ 1000)                            
+            xx += avgw        
+        
+        if smart == 1:   #smart avg  
+            if avgw.T.dot(x) * sign <= 0:                
+                avgerror += 1 
+                avgw += sign * x               
+                xx += sign * x * avgcount   
+
+                if bestratea > (float(avgerror) / lines):                    
+                    bestratea = (float(avgerror) / lines)                    
+                    besttimea = (float(lines) / 1000)
+
         if lines % 1000 == 0:
             if smart == 0:
-                avgw = (avgw + (xx / avgcount) ) / 2
+                avgw = xx / avgcount
             if smart == 1:
-                # print "before: ",avgw
-                # print "avgcount", xx / avgcount
                 avgw = avgw - (xx / avgcount)
-                # print "after",avgw
-            print  "error= %5s/%5s" % (error, lines), "rate: %.5f" % (float(error) / lines), "avg error= %5s/%5s" % (avgerror, lines), "rate:", float(avgerror) / lines
-            avgcount = 0
-            xx = np.zeros(261)
+
+            print  "Iter %5.2f" % (float(lines)/ 1000),\
+            "error rate: %.5f" % (float(error) / lines),\
+            "avg error rate: %.5f" % (float(avgerror) / lines),
+            bt,br,bta,bra = pladev('income.dev.txt',w,dictdata,avgw)
+            if bestratea > bra:                    
+                bestratea = bra                    
+                besttimea = bta
+            if bestrate > br:                
+                bestrate = br             
+                besttime = bt
+
+            if lines % 5000 == 0:
+                print ""
+
+            
         avgcount += 1
-        lines+= 1
-        
-    if smart == 0:
-        avgw = (avgw + (xx / 1000) ) / 2
-    if smart == 1:
-        print avgcount
-        avgw = avgw - xx / avgcount   
-    print  "error= %5s/%5s" % (error, lines), "rate: %.5f" % (float(error) / lines), "avg error= %5s/%5s" % (avgerror, lines), "rate:", float(avgerror) / lines
-    print "w: ",w
-    # print "avgw: ",avgw
-    # print ""
+        lines += 1
+             
+    print "best"
+    print "Iter %5.3f" % besttime,  "error rate: %.5f" % bestrate
+    print "Iter %5.3f" % besttimea, "avg error rate: %.5f" % bestratea
+    # print "w: ",w
+
     return w,avgw
 
-def mira(file,w,dictdata,binfea,avgw):
+def pladev(file,w,dictdata,avgw):
+    f = open(file,'r')
+    smart = 0
+    bestw = w
+    bestrate = 90.0
+    besttime = 0.0
+    bestratea = 90.0
+    besttimea = 0.0
+    totalline = 0
+    for line in f: 
+        totalline += 1
+    # print totalline
+
+    f = open(file,'r')
     error = 0
     avgerror = 0
-    f = open(file,'r')
-    x = np.zeros(261)
-    xx = np.zeros(261)
-    count = 1
-    lines = 1
-    bias = 0
-    sign = 0
-    smart = 1
+    x = np.zeros(260)
+    xx = np.zeros(260)
+    count = 0 #diction num
     avgcount = 1
-    dist = 0.0
-    aggr = 1
-    p = 0.2
-    for line in f:
+    lines = 1
+    sign = 0
+    for line in f:  ## set data
+        
         frame = line.split(", ")
-        elements = 0
-        x = np.zeros(261)
-        # print frame
+        x = np.zeros(260)
         if frame[9] == "<=50K\n":
             sign = -1
         if frame[9] == ">50K\n":
             sign = 1
         frame[0] = "Y" +frame[0]
         frame[7] = "Hr"+frame[7]
-
-        for element in frame:
+        x.fill(-1)
+        # print frame
+        # time.sleep(1)
+        for element in frame[:9]:
             if element not in dictdata:
                 dictdata[element] = count
-                binfea[element] = 0
                 count += 1
-            
-            if sign == 1:
-                binfea[element] += 1
-            else:
-                binfea[element] -= 1
-            x[dictdata[element]] = 1
-            # if binfea[element] > 0:
-            #     x[elements] = 1
-            # if binfea[element] < 0:
-            #     x[elements] = 0
-            
-            elements += 1
+            x[dictdata[element]] = 1    
 
+        if lines == 1000:            
+            avgw = w                
 
-        # x[0] = dictdata[frame[0]]
-        # x[7] = dictdata[frame[7]]
-        # x[9] = 1
-        # x[9] = np.linalg.norm(x)
-        np.set_printoptions(formatter={'float': '{: 6.1f}'.format})
-        # print "x: ",x , sign
-        # print "avgw: ",avgw
-        # print "xx: ",xx
-
-        if lines == 100:
-            avgw = w
-        
-        if int(np.sign(w.T.dot(x))) != sign:
-            error += 1
-            dist = np.linalg.norm(x)
-            w += x * (sign - w.T.dot(x)) / dist*dist 
-            # print "xb:",x
-            # print "xa:",x * (sign - w.T.dot(x)) / dist*dist 
-        if int(np.sign(w.T.dot(x))) == sign and aggr == 1 and w.T.dot(x) * sign <= p:
-            # error += 1
+        if w.T.dot(x) * sign <= 0:             
+            error += 1            
+            if bestrate > (float(error) / lines):                
+                bestrate = (float(error) / lines)                
+                besttime = (float(lines)/ 1000)            
+            w += sign * x    
             
-            dist = np.linalg.norm(x)
-            print dist
-            w += x * (sign - w.T.dot(x)) / dist*dist 
-        
-        if smart == 0:
-            if int(np.sign(avgw.T.dot(x))) != sign:
+             
+        if smart == 0:            
+            if avgw.T.dot(x) * sign <= 0:                
                 avgerror += 1
-            xx += w
-        if smart == 1:
-            if int(np.sign(avgw.T.dot(x))) != sign:
-                avgerror += 1
-                xx += sign * x * avgcount * (sign - avgw.T.dot(x)) / dist*dist
+                avgw += sign * x                
+                if bestratea > (float(avgerror) / lines):                    
+                    bestratea = (float(avgerror) / lines)                    
+                    besttimea = (float(lines)/ 1000)                            
+            xx += avgw        
         
-        if lines % 1000 == 0:
-            if smart == 0:
-                avgw = (avgw + (xx / avgcount) ) / 2
-            if smart == 1:
-                # print "before: ",avgw
-                # print "avgcount", xx / avgcount
-                avgw = avgw - (xx / avgcount)
-                # print "after",avgw
-            print  "mira error= %5s/%5s" % (error, lines), "rate: %.5f" % (float(error) / lines), "avg error= %5s/%5s" % (avgerror, lines), "rate:", float(avgerror) / lines
-            avgcount = 0
-            xx = np.zeros(261)
+        if smart == 1:            
+            if avgw.T.dot(x) * sign <= 0:                
+                avgerror += 1 
+                avgw += sign * x               
+                xx += sign * x * avgcount   
+
+                if bestratea > (float(avgerror) / lines):                    
+                    bestratea = (float(avgerror) / lines)                    
+                    besttimea = (float(lines) / 1000)
+
             
         avgcount += 1
-        lines+= 1
-        
+        lines += 1
+
     if smart == 0:
-        avgw = (avgw + (xx / 1000) ) / 2
+        avgw = xx / avgcount
     if smart == 1:
-        print avgcount
-        avgw = avgw - xx / avgcount   
-    print  "mira error= %5s/%5s" % (error, lines), "rate: %.5f" % (float(error) / lines), "avg error= %5s/%5s" % (avgerror, lines), "rate:", float(avgerror) / lines
-    # print "w: ",w
-    # print "avgw: ",avgw
-    # print ""
-    xx = np.zeros(261)
-    return w,avgw
+        avgw = avgw - (xx / avgcount)
+
+    print "dev error rate: %.5f" % (float(error) / lines),\
+    "avg rate: %.5f" % (float(avgerror) / lines)
+
+    return besttime,bestrate,besttimea,bestratea
 
 def main():
-    # file = 'income.train.txt'
-    # w = np.zeros(10)
-    # avgw = np.zeros(10)
-    # dictdata = {}
-    # binfea = {}
-    # for x in xrange(0,5):
-    #     w,avgw = pla1(file,w,dictdata,binfea,avgw)
-    # file = 'income.dev.txt'
-    # print "dev"
-    # w,avgw = pla1(file,w,dictdata,binfea,avgw)
-
     file = 'income.train.txt'
-    w = np.zeros(10)
-    avgw = np.zeros(10)
+    w = np.zeros(260)
+    avgw = np.zeros(260)
     dictdata = {}
-    binfea = {}
-    for x in xrange(0,5):
-        w,avgw = mira(file,w,dictdata,binfea,avgw)
-    file = 'income.dev.txt'
-    # print "dev"
-    # w,avgw = mira(file,w,dictdata,binfea,avgw)
+    iteration = 5
+    mira = 2
+    p = 0.2
+    w,avgw = pla1(file,w,dictdata,avgw,iteration,mira,p)
+
+    print w
+
     
 
 if __name__ == '__main__':
-    # main()
-    file = 'income.train.txt'
-    w = np.zeros(261)
-    avgw = np.zeros(261)
-    dictdata = {}
-    binfea = {}
-    w, avgw = pla1(file,w,dictdata,binfea,avgw)
-    # w, avgw = mira(file,w,dictdata,binfea,avgw)
-    # print binfea
+    main()
     
         
     
