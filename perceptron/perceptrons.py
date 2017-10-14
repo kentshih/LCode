48861717e91352143024f10ced5e5cef202cfc0d
Kent Shih <shihm@oregonstate.edu>
2017-10-13 23:34:34 -0700

    perceptron mira optimal

--- perceptron/perceptrons.py
+++ perceptron/perceptrons.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 
@@ -14,7 +14,12 @@ def pla1(file,w,dictdata,avgw,iteration,mira,p):
     bestratea = 90.0
     besttimea = 0.0
 
-    f = open(file,'r')
+    xaxis = []
+    yaxis1 = []
+    yaxis2 = []
+    yaxis3 = []
+    yaxis4 = []
+
     error = 0
     avgerror = 0
     x = np.zeros(260)
@@ -41,7 +46,7 @@ def pla1(file,w,dictdata,avgw,iteration,mira,p):
                 count += 1
             x[dictdata[element]] = 1    
 
-        if lines == 1000:            
+        if lines == 0:            
             avgw = w                
 
         if w.T.dot(x) * sign <= 0:      #PLA
@@ -52,8 +57,6 @@ def pla1(file,w,dictdata,avgw,iteration,mira,p):
             w += sign * x
             dist = np.linalg.norm(x)
             # print (sign - w.T.dot(x)),"/", dist*dist,"=", (sign - w.T.dot(x)) / float(dist*dist)
-            # time.sleep(1)
-            # print mira
             # time.sleep(1)
             if mira >= 1:
                 w += x * (sign - w.T.dot(x)) / float(dist*dist)
@@ -78,18 +81,35 @@ def pla1(file,w,dictdata,avgw,iteration,mira,p):
 
                 if bestratea > (float(avgerror) / lines):                    
                     bestratea = (float(avgerror) / lines)                    
-                    besttimea = (float(lines) / 1000)
+                    besttimea = (float(lines) / 1000) 
+
+        # if smart == 0:
+        #     avgw = xx / avgcount
+        # if smart == 1:
+        #     avgw = avgw - (xx / avgcount)
+        if lines % 200 == 0:
+            xaxis.append(float(lines))
+            yaxis1.append(float(error) / lines)
+            yaxis2.append(float(avgerror) / lines)
+            bt,br,bta,bra,temp1,temp2 = pladev('income.dev.txt',w,dictdata,avgw,mira,p)
+            yaxis3.append(temp1)
+            yaxis4.append(temp2)
 
         if lines % 1000 == 0:
             if smart == 0:
                 avgw = xx / avgcount
             if smart == 1:
                 avgw = avgw - (xx / avgcount)
-
+            # xaxis.append(lines)
+            # yaxis1.append(float(error) / lines)
+            # yaxis2.append(float(avgerror) / lines)
+            
             print  "Iter %5.2f" % (float(lines)/ 1000),\
             "error rate: %.5f" % (float(error) / lines),\
             "avg error rate: %.5f" % (float(avgerror) / lines),
-            bt,br,bta,bra = pladev('income.dev.txt',w,dictdata,avgw)
+            bt,br,bta,bra,temp1,temp2 = pladev('income.dev.txt',w,dictdata,avgw,mira,p)
+            # yaxis3.append(temp1)
+            # yaxis4.append(temp2)
             if bestratea > bra:                    
                 bestratea = bra                    
                 besttimea = bta
@@ -98,31 +118,52 @@ def pla1(file,w,dictdata,avgw,iteration,mira,p):
                 besttime = bt
 
             if lines % 5000 == 0:
+                print "best"
+                print "Iter %5.3f" % besttime,  "error rate: %.5f" % bestrate
+                print "Iter %5.3f" % besttimea, "avg error rate: %.5f" % bestratea
                 print ""
 
             
         avgcount += 1
         lines += 1
              
-    print "best"
+    print "best dev"
     print "Iter %5.3f" % besttime,  "error rate: %.5f" % bestrate
     print "Iter %5.3f" % besttimea, "avg error rate: %.5f" % bestratea
     # print "w: ",w
 
+    plt.plot(xaxis,yaxis1,color = "blue",linewidth = 2.5,linestyle = "-",label = "mira")
+    plt.plot(xaxis,yaxis2,color = "green",linewidth = 2.5,linestyle = "-",label = "avg mira")
+    plt.plot(xaxis,yaxis3,color = "red",linewidth = 2.5,linestyle = "-",label = "dev mira")
+    plt.plot(xaxis,yaxis4,color = "black",linewidth = 2.5,linestyle = "-",label = "dev avg mira")
+    # plt.plot(xaxis,yaxis1,color = "blue",linewidth = 2.5,linestyle = "-",label = "perceptron")
+    # plt.plot(xaxis,yaxis2,color = "green",linewidth = 2.5,linestyle = "-",label = "avg perceptron")
+    # plt.plot(xaxis,yaxis3,color = "red",linewidth = 2.5,linestyle = "-",label = "dev perceptron")
+    # plt.plot(xaxis,yaxis4,color = "black",linewidth = 2.5,linestyle = "-",label = "dev avg perceptron")
+    plt.legend(loc='upper left')
+    plt.xlim(0, 5000)
+    plt.ylim(0.1, 0.4)
+    plt.title('AMIRA 0.1')
+    plt.show()
+
     return w,avgw
 
-def pladev(file,w,dictdata,avgw):
+def pladev(file,w,dictdata,avgw,mira,p):
     f = open(file,'r')
-    smart = 0
+    smart = 1
     bestw = w
     bestrate = 90.0
     besttime = 0.0
     bestratea = 90.0
     besttimea = 0.0
     totalline = 0
+
     for line in f: 
         totalline += 1
     # print totalline
+    xaxis = []
+    yaxis3 = []
+    yaxis4 = []
 
     f = open(file,'r')
     error = 0
@@ -152,16 +193,21 @@ def pladev(file,w,dictdata,avgw):
                 count += 1
             x[dictdata[element]] = 1    
 
-        if lines == 1000:            
+        if lines % 1000 == 0:            
             avgw = w                
-
+        dist = np.linalg.norm(x)
         if w.T.dot(x) * sign <= 0:             
             error += 1            
             if bestrate > (float(error) / lines):                
                 bestrate = (float(error) / lines)                
                 besttime = (float(lines)/ 1000)            
-            w += sign * x    
+            w += sign * x
             
+            if mira >= 1:
+                w += x * (sign - w.T.dot(x)) / float(dist*dist)    
+        
+        if w.T.dot(x) * sign > 0 and mira == 2 and w.T.dot(x) * sign <= p:
+            w += x * (sign - w.T.dot(x)) / float(dist*dist)    
              
         if smart == 0:            
             if avgw.T.dot(x) * sign <= 0:                
@@ -182,6 +228,10 @@ def pladev(file,w,dictdata,avgw):
                     bestratea = (float(avgerror) / lines)                    
                     besttimea = (float(lines) / 1000)
 
+        if lines % 200 == 0:
+            xaxis.append(float(lines))
+            yaxis3.append(float(error) / lines)
+            yaxis4.append(float(avgerror) / lines)
             
         avgcount += 1
         lines += 1
@@ -194,7 +244,7 @@ def pladev(file,w,dictdata,avgw):
     print "dev error rate: %.5f" % (float(error) / lines),\
     "avg rate: %.5f" % (float(avgerror) / lines)
 
-    return besttime,bestrate,besttimea,bestratea
+    return besttime,bestrate,besttimea,bestratea,(float(error) / lines),(float(avgerror) / lines)
 
 def main():
     file = 'income.train.txt'
@@ -203,10 +253,10 @@ def main():
     dictdata = {}
     iteration = 5
     mira = 2
-    p = 0.2
+    p = 0.1
     w,avgw = pla1(file,w,dictdata,avgw,iteration,mira,p)
 
-    print w
+    # print w
 
     
 
