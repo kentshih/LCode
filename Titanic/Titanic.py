ab77844ac5f523323fdfcb50a63bec98bdbe41e9
Kent Shih <shihm@oregonstate.edu>
2017-11-27 01:08:43 -0800

    titanic

--- Titanic/Titanic.py
+++ Titanic/Titanic.py
@@ -2,6 +2,8 @@
 #-*- coding:utf-8 -*-
 import csv
 import os
+import pandas as pd
+import numpy as np
 from sklearn import svm
 
 def readData(fileName):
@@ -19,10 +21,97 @@ def readData(fileName):
                 result['attr_list'] = row
     return result
 
-def main():
-    filename = 'train.csv'
+def writeData(fileName, data):
+    csvFile = open(fileName, 'w')
+    writer = csv.writer(csvFile)
+    n = len(data)
+    for i in range(n):
+        writer.writerow(data[i])
+    csvFile.close()
 
-    print readData(filename)
+def convertData(dataList):
+    hashTable = {}
+    count = 0.0
+    for i in range(len(dataList)):
+        if not hashTable.has_key(dataList[i]):
+            hashTable[dataList[i]] = count
+            count += 1
+        dataList[i] = hashTable[dataList[i]]
+
+def convertValueData(dataList):
+    sumValue = 0.0
+    count = 0
+    for i in range(len(dataList)):
+        if dataList[i] == "":
+            continue
+        sumValue += float(dataList[i])
+        count += 1
+        dataList[i] = float(dataList[i])
+    avg = sumValue / count
+    for i in range(len(dataList)):
+        if dataList[i] == "":
+            dataList[i] = avg
+
+def dataPredeal(data):
+    convertValueData(data["Age"])
+    convertData(data["Fare"])
+    convertData(data["Pclass"])
+    convertData(data["Sex"])
+    convertData(data["SibSp"])
+    convertData(data["Parch"])
+    convertData(data["Embarked"])
+  
+def getX(data): 
+    x = []
+    ignores = {"PassengerId":1, "Survived":1, "Name":1,"Ticket":1, "Cabin":1, "Fare":1, "Embarked":1}    
+    for i in range(len(data["PassengerId"])):
+        x.append([])
+        for j in range(len(data["attr_list"])):
+            key = data["attr_list"][j]
+            if not ignores.has_key(key):
+                x[i].append(data[key][i])
+    return x
+
+def getLabel(data):
+    label = []
+    for i in range(len(data["PassengerId"])):
+        label.append(int(data["Survived"][i]))
+    return label
+
+def calResult(x,label, input_x):
+    svmcal = svm.SVC(kernel='linear').fit(x, label)
+    return svmcal.predict(input_x)
+
+def run():
+    dataRoot = 'input/'
+    data = readData(dataRoot + 'train.csv')
+    test_data = readData(dataRoot + 'test.csv')
+    dataPredeal(data)
+    dataPredeal(test_data)
+    x = getX(data)
+    label = getLabel(data)
+    input_x = getX(test_data)
+    x_result = calResult(x, label,input_x)
+    res = [[test_data["PassengerId"][i], x_result[i]] for i in range(len(x_result))]
+    res.insert(0, ["PassengerId", "Survived"])
+    writeData(dataRoot + 'result.csv', res)
+
+def main():
+    filename = "input/train.csv"
+
+    result = readData(filename)
+    # get titanic & test csv files as a DataFrame
+    train = pd.read_csv("input/train.csv")
+    test  = pd.read_csv("input/test.csv")
+
+    full = train.append( test , ignore_index = True )
+    titanic = full[ :891 ]
+
+    del train , test
+
+    print ('Datasets:' , 'full:' , full.shape , 'titanic:' , titanic.shape)
+    titanic.head()
+    titanic.describe()
 
 if __name__ == '__main__':
-    main()
+    run()
