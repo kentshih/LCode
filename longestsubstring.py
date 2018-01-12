581f351eedb9233eb9b46fc74adb118327b6d525
Kent Shih <shihm@oregonstate.edu>
2018-01-11 23:48:11 -0800

    longest sub loop

--- longestsubstring.py
+++ longestsubstring.py
@@ -7,19 +7,21 @@ class Solution(object):
         Lstr = ""
         Tstr = ""
         pos = 0
-        for i,c in enumerate(s,1):
-            Tstr = c
-            pos = i-1
+        # for i,c in enumerate(s,1):
+        Tstr = s[0]
+        pos = 0
+        i = 1
+        while i < len(s):
             for j in xrange(i,len(s)):
                 if s[j] not in Tstr:
                     if j == pos+1:
                         Tstr += s[j]
                         pos += 1
                 else:
+                    if len(Tstr) > len(Lstr):
+                        Lstr = Tstr
+                    i = pos
+                    Tstr = s[i]
                     break
-            print c, i, Tstr
-            
-            if len(Tstr) > len(Lstr):
-                Lstr = Tstr
-        
+
         return len(Lstr)
