32724a02db46f19b70d672493a0b01863355f54d
Kent Shih <shihm@oregonstate.edu>
2018-02-08 23:52:56 -0800

    palindromic

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
+        ans = ""
+        n = len(s)
+        i = 0
+        j = n - 1
+        while i < j:
+            if s[i] == s[j]:
+                start = i
+                end = j
+                while i != j:
+                    if s[i] == s[j]:
+                        i += 1
+                        j -= 1
+                        continue
+                    else:
+                        break
+                if s[i] == s[j]:
+                    if len(ans) < end - start:
+                        ans = s[start:end]
+            i += 1
+        return ans
