b0ba2c94976d1c2931b03629d4764f6aa4aa42d1
Kent Shih <shihm@oregonstate.edu>
2018-02-13 22:44:06 -0800

    palindromic

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -26,6 +26,6 @@ class Solution(object):
             i += 1
         return stack
     def helper(self, s, l, r):
-    while l >= 0 and r < len(s) and s[l] == s[r]:
-        l -= 1; r += 1
-    return s[l+1:r]
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
