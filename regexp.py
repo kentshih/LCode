f0e7bf926bfbfaa357d84808c323a3ae3dc24424
Kent Shih <shihm@oregonstate.edu>
2018-02-23 13:55:00 -0800

    reg expr

--- regexp.py
+++ regexp.py
@@ -8,15 +8,16 @@ class Solution(object):
         count = 0
         n = len(s)
         m = len(p)
-        table = [[True] + [False]*m]
-        for i in xrange(n):
-            table.append([False]*(m+1))
+        table = [[True] + [False]*n]
+        for i in xrange(m):
+            table.append([False]*(n+1))
         
-        for i in xrange(1, n+1):
+        for i in xrange(1, m+1):
+            print i
             cur = p[i-1]
             if cur == '*' and i > 1:
                 table[i][0] = table[i-2][0]
-            for j in xrange(1, m+1):
+            for j in xrange(1, n+1):
                 if cur == '*':
                     table[i][j] = table[i-2][j] or \
                                   table[i-1][j] or \
@@ -24,4 +25,4 @@ class Solution(object):
                                  (table[i]  [j-1] and p[i-2] == '.' )
                 elif cur == '.' or cur == s[j-1]:
                     table[i][j] = table[i-1][j-1]
-        return table[n][m]
+        return table[m][n]
