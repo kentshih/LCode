2da9c9c712b717f3317caa16d4ae76c8c4282437
Kent Shih <shihm@oregonstate.edu>
2018-02-24 15:09:46 -0800

    Most Water

---
+++ MostWater.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n = len(height)
+        Mans = ans = 0
+        l = 0
+        r = n-1
+        dp = []
+        while l < r:
+            left, right = height[l], height[r]
+            if left < right:
+                ans = (r - l) * left
+                while height[l] <= left:
+                    l += 1
+            
+            else:
+                ans = (r - l) * right
+                while height[r] <= right and r >= 0:
+                    r -= 1
+            if ans > Mans:
+                Mans = ans
+        return Mans
+            
