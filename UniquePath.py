a993f6820ef778ca73d2b6c71954d00720232005
Kent Shih <shihm@oregonstate.edu>
2018-03-15 21:38:14 -0700

    Unique Path

--- UniquePath.py
+++ UniquePath.py
@@ -5,4 +5,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        ans = 0
+        if not m or not n:
+            return 0
+        cur = [1] * n
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
