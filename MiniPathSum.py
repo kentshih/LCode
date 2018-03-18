c7e4484ab49d06291214e0a36f3925ce2f2f58b7
Kent Shih <shihm@oregonstate.edu>
2018-03-17 23:10:31 -0700

    Mini Path Sum

--- MiniPathSum.py
+++ MiniPathSum.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        m = len(grid)
+        if m != 0:
+            n = len(grid[0])
+        ans = grid[0][0]
+        dp = [[y for j in xrange(m)] for i in xrange(n)]
+        
+        while 
+        
         
