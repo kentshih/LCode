026aa88f4c50294f171a4f2384e68c566b71ba80
Kent Shih <shihm@oregonstate.edu>
2018-03-21 21:45:26 -0700

    Climb stairs

--- ClimbStairs.py
+++ ClimbStairs.py
@@ -4,10 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [x for i in range(n)]
+        dp = [i for i in range(n)]
         dp[0] = 1
-        for i in xrange(1,n):
-            dp[i] = dp[i-1] + 1
-        
+        if n > 1:
+            dp[1] = 2
+        for i in xrange(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
             
         return dp[-1]
