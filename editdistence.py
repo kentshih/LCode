aa988f451c0388dd9878bd71926e572c234b9d96
Kent Shih <shihm@oregonstate.edu>
2018-03-28 22:55:05 -0700

    Edit distence

--- editdistence.py
+++ editdistence.py
@@ -8,7 +8,34 @@ class Solution(object):
         n = len(word1)
         m = len(word2)
         
-        dp = [x for i in xrange(n)]
-        dp2 = []
-        for i in range(m):
-            dp2.append(dp)
+        dp = [999 for i in xrange(n+1)]
+        dp2 = [dp[:] for i in xrange(m+1)]
+        if m == 0:
+            return n
+        if n == 0:
+            return m
+        dp2[0][0] = 0
+        # print dp2
+        for i in xrange(1,n+1):
+            if word1[i-1] == word2[0]:
+                dp2[0][i] = dp2[0][i-1]
+            else:
+                dp2[0][i] = dp2[0][i-1] + 1
+        for j in xrange(1,m+1):
+            if word1[0] == word2[j-1]:
+                dp2[j][0] = dp2[j-1][0]
+            else:
+                dp2[j][0] = dp2[j-1][0] + 1
+        # print dp2
+        for i in xrange(1,n+1):
+            for j in xrange(1,m+1):
+                
+                if word1[i-1] == word2[j-1]:
+                    dp2[j][i] = dp2[j-1][i-1]
+                else:
+                    dp2[j][i] = dp2[j-1][i-1] + 1
+                # print i, j
+                dp2[j][i] = min(dp2[j-1][i] + 1,dp2[j][i-1] + 1, dp2[j][i])
+        print dp2
+        return dp2[m][n]
+                    
