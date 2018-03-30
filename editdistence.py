532f86dba590e44d7d53825f5cace9e1d342783f
Kent Shih <shihm@oregonstate.edu>
2018-03-29 21:18:47 -0700

    Edit distence

--- editdistence.py
+++ editdistence.py
@@ -7,35 +7,16 @@ class Solution(object):
         """
         n = len(word1)
         m = len(word2)
+        if n == 0 or m == 0:
+            return max(m, n)
+        dist = range(m + 1)
+        for i in xrange(n):
         
-        dp = [999 for i in xrange(n+1)]
-        dp2 = [dp[:] for i in xrange(m+1)]
-        if m == 0:
-            return n
-        if n == 0:
-            return m
-        dp2[0][0] = 0
-        # print dp2
-        for i in xrange(1,n+1):
-            if word1[i-1] == word2[0]:
-                dp2[0][i] = dp2[0][i-1]
-            else:
-                dp2[0][i] = dp2[0][i-1] + 1
-        for j in xrange(1,m+1):
-            if word1[0] == word2[j-1]:
-                dp2[j][0] = dp2[j-1][0]
-            else:
-                dp2[j][0] = dp2[j-1][0] + 1
-        # print dp2
-        for i in xrange(1,n+1):
-            for j in xrange(1,m+1):
-                
-                if word1[i-1] == word2[j-1]:
-                    dp2[j][i] = dp2[j-1][i-1]
+            dist_ij, dist[0] = i, i + 1
+            for j in xrange(m):
+                if word1[i] == word2[j]:
+                    dist_ij, dist[j + 1] = dist[j + 1], dist_ij
                 else:
-                    dp2[j][i] = dp2[j-1][i-1] + 1
-                # print i, j
-                dp2[j][i] = min(dp2[j-1][i] + 1,dp2[j][i-1] + 1, dp2[j][i])
-        print dp2
-        return dp2[m][n]
+                    dist_ij, dist[j + 1] = dist[j + 1], min(dist[j], dist[j + 1], dist_ij) + 1
+        return dist[-1]
                     
