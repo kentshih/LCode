1afb2ed0657e9055b139f3472ce228fcc46f49c1
Kent Shih <shihm@oregonstate.edu>
2018-04-01 23:14:27 -0700

    Sort color

--- sortcolors.py
+++ sortcolors.py
@@ -7,8 +7,18 @@ class Solution(object):
         n = len(nums)
         if n == 0:
             return
+        start = 0
         cur = 0
         end = n - 1
         while cur <= end and cur < n:
-            
+            if nums[cur] == 0:
+                nums[cur], nums[start] = nums[start], 0
+                cur += 1
+                start += 1
+            elif nums[cur] == 1:
+                cur += 1
+            elif nums[cur] == 2:
+                nums[cur], nums[end] = nums[end], 2
+                end -= 1
+                
             
