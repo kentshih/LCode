9a0c0971f727db653a80b587250441c429228464
Kent Shih <shihm@oregonstate.edu>
2018-04-17 23:32:26 -0700

    Minimum Window

--- minwindow.py
+++ minwindow.py
@@ -23,3 +23,16 @@ class Solution(object):
                 missing -= 1
             if c in lt:
                 lt[c] -= 1
+
+            while i < j and not missing:
+                if not J or j-i < J-I:
+                    I, J = i, j
+                if s[i] not in lt:
+                    i += 1
+                    continue
+                else:
+                    lt[s[i]] += 1
+                    if lt[s[i]] > 0:
+                        missing += 1
+                    i += 1
+        return s[I:J]
