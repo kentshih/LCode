2a440975c3dd3383801e93be8b1178b8fd694583
Kent Shih <shihm@oregonstate.edu>
2018-05-14 20:54:00 +0800

    Word search

--- wordsearch.py
+++ wordsearch.py
@@ -6,6 +6,28 @@ class Solution(object):
         :rtype: bool
         """
         m, n  = len(board), len(board[0])
+    def is_valid(self, i, j, board):
+        m, n  = len(board), len(board[0])
         if i < 0 or j < 0 or i >= m or j >= n:
             return False
         return True
+
+    def dfs(self, i, j, k, visited, board, word):
+        if (i, j) in visited:
+            return False
+
+        if board[i][j] != word[k]:
+            return False
+
+        if k + 1 == len(word):
+            return True
+
+        visited.add((i, j))
+        directions = {(1, 0), (-1, 0), (0, 1), (0, -1)}
+        for d in directions:
+            ni, nj = i + d[0], j + d[1]
+            if self.is_valid(ni, nj, board) and self.dfs(ni, nj, k+1, visited, board, word):
+                return True
+        visited.remove((i, j))
+
+        return False
