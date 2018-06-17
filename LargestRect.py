9679782d611ecd18fcd365d9d110ff7612b6d88a
Kent Shih <shihm@oregonstate.edu>
2018-06-16 23:52:08 -0700

    Largest Rect Histogram

--- LargestRect.py
+++ LargestRect.py
@@ -7,4 +7,11 @@ class Solution(object):
         n = len(heights)
         stack = []
         temp = []
-        
+        heights.append(float('-inf'))
+        for i,h in enumerate(heights):
+            cpos = i
+            while stack and h < stack[-1][0]:
+                ch, cpos = stack.pop()
+                temp.append(ch*(i - cpos ))
+            stack.append([h, cpos])
+        return max(temp) if temp else 0
