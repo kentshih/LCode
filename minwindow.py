c607c8b49d2c05e74ecff547141626ec8687f82d
Kent Shih <shihm@oregonstate.edu>
2018-06-20 23:11:25 -0700

    mini window spark

--- minwindow.py
+++ minwindow.py
@@ -1,38 +1,35 @@
 class Solution(object):
     def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        m = len(s)
-        n = len(t)
-        if m < n:
-            return ''
-        lt = {}
+        if len(s) < len(t):
+            return ""
         
-        for i in t:
-            if i not in lt:
-                lt[i] = 1
-            else:
-                lt[i] += 1
-        missing = n
-        i = I = J = 0
-        for j, c in enumerate(s, 1):    
-            if c in lt and lt[c] > 0:
-                missing -= 1
-            if c in lt:
-                lt[c] -= 1
-
-            while i < j and not missing:
-                if not J or j-i < J-I:
-                    I, J = i, j
-                if s[i] not in lt:
-                    i += 1
-                    continue
-                else:
-                    lt[s[i]] += 1
-                    if lt[s[i]] > 0:
-                        missing += 1
-                    i += 1
-        return s[I:J]
+        l, r = 0, 0
+        c = collections.Counter(t) # original counter
+        w = collections.Counter()  # window   counter
+        missing = len(t)           # number of characters missing from the window
+        best = ""
+        
+        while True:
+            # go right until we have all characters needed
+            while missing:
+                if r == len(s):
+                    return best
+                
+                if s[r] in c:
+                    w[s[r]] += 1
+                    if w[s[r]] <= c[s[r]]:
+                        missing -= 1
+                r += 1
+            
+            # remove characters from the left until we don't have enough characters anymore
+            while not missing:
+                # skip the bullshit
+                while s[l] not in c:
+                    l += 1
+                # compare to and save best solution
+                if r-l < len(best) or not best:
+                    best = s[l:r]
+                w[s[l]] -= 1
+                if w[s[l]] < c[s[l]]:
+                    missing += 1
+                l += 1
