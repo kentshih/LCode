e82962133633fbfcbbe86e940aee3faf59e2f97a
Kent Shih <shihm@oregonstate.edu>
2018-06-27 22:38:40 -0700

    Swift

--- Helloworld/Helloworld/ViewController.swift
+++ Helloworld/Helloworld/ViewController.swift
@@ -20,11 +20,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome!", message: "Hello World!", preferredStyle: <#T##UIAlertControllerStyle#>)
-        alertController.addAction(UIAlertAction(title: <#T##String?#>, style: <#T##UIAlertActionStyle#>, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>))
-        present(alertController, animated: true, completion: nil)
-    }
-
 }
-
