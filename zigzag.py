4e0f5b23c787846418d5d9045a207f46b4bda5d7
Kent Shih <shihm@oregonstate.edu>
2018-07-21 23:13:04 -0700

    zigzag convert

--- zigzag.py
+++ zigzag.py
@@ -10,5 +10,11 @@ class Solution(object):
         if numRows < 2 or n < numRows: 
             return s
         else:
-            for i in range(numRows):
-                k=0 # initial column
+            for i in xrange(numRows):
+                k = 0
+                while k < n:
+                    if k % (2*numRows - 2) == i:
+                        zig_zag += s[k]
+                        k += k
+                        continue
+                    
