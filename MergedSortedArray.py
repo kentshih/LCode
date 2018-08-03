74258d37321f5de7880eea9aeebbf21149529788
Kent Shih <shihm@oregonstate.edu>
2018-08-02 23:55:26 -0700

    Merged sorted array

--- MergedSortedArray.py
+++ MergedSortedArray.py
@@ -7,12 +7,26 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-        i = 0
-        end = nums1[m-1]
-        for j in range(len(nums2)):
-            if nums1[i] == end: # concat remaining 
-                nums1 += nums2[j:]
-                break
-            if nums2[j] < nums1[i]: 
-                nums1 = nums1[i:] + [nums2[j]] + nums1[:i]
-                i += 1
+        if n > 0:
+            
+            i = m-1
+            j = n-1
+            end = nums1[m-1]
+            for k in range(m+n-1,-1,-1):
+                if nums1[i] > nums2[j]: # put nums1  
+                    nums1[k], nums1[i] = nums1[i], nums1[k]
+                    i -= 1
+                elif nums2[j] >= nums1[i]: # put nums2 
+                    print nums2[j] 
+                    nums1[k] = nums2[j] 
+                    j -= 1
+                print nums1
+                if j == -1:
+                    print "j end"
+                    break
+                if i == -1:
+                    print i,j,k
+                    nums1 = nums2[:j+1] + nums1[k:]
+                    print "i end"
+                    break
+                
