965cbffe8c6d98db21ddbda6361d8644c8e63213
Kent Shih <shihm@oregonstate.edu>
2018-08-10 15:49:04 -0700

    Find min in rotated sorted array

--- Findmin.py
+++ Findmin.py
@@ -5,25 +5,22 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
+        if n == 0:
+            return 0
         if n == 1:
             return nums[0]
+        if n == 2:
+            return min(nums[0], nums[1])
         start = 0
-        ans = 999
         end = n - 1
-        while start+1 < end:
+        while start < end - 1:
+            if nums[start] < nums[end]:
+                return nums[start]
             mid = (start + end) / 2
-            
+            if (start + end) % 2 == 1:
+                mid += 1  
             if nums[start] > nums[mid]:
-                start = mid
-                continue
-            if nums[start] < nums[mid]:
                 end = mid
-                continue
-        print start, end
-        mid = (start + end) / 2
-        if (start + end) % 2 == 1:
-            mid += 1
-        if nums[start] < nums[end]:
-            return nums[start]
-        else:
-            return nums[end]
+            elif nums[start] < nums[mid]:
+                start = mid
+        return min(nums[start],nums[end])
