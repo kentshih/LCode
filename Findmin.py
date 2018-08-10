7ff94175ae50262a4eb5f985b5e932d8117c5122
Kent Shih <shihm@oregonstate.edu>
2018-08-09 23:40:07 -0700

    Find min in rotated sorted array

--- Findmin.py
+++ Findmin.py
@@ -12,13 +12,17 @@ class Solution(object):
         end = n - 1
         while start+1 < end:
             mid = (start + end) / 2
-            print start, end
+            
             if nums[start] > nums[mid]:
-                end = mid
-                continue
-            if nums[start] < nums[mid]:
                 start = mid
                 continue
+            if nums[start] < nums[mid]:
+                end = mid
+                continue
+        print start, end
+        mid = (start + end) / 2
+        if (start + end) % 2 == 1:
+            mid += 1
         if nums[start] < nums[end]:
             return nums[start]
         else:
