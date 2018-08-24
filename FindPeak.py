96d62db120505921cdf75823c21b7b423162da46
Kent Shih <shihm@oregonstate.edu>
2018-08-23 21:59:17 -0700

    find peak

--- FindPeak.py
+++ FindPeak.py
@@ -9,11 +9,15 @@ class Solution(object):
             return -1
         if n == 1:
             return 0
-        end  = n - 1
+        end   = n - 1
         start = 0
-        while start < end:
+        while start <= end:
             mid = ( start + end ) / 2
-            if mid != 0:
-                if nums[mid] > nums[mid-1]:
-                    
-            
+            if (mid == 0     or nums[mid] >= nums[mid - 1]) and \
+               (mid == n - 1 or nums[mid] >= nums[mid + 1]):
+                return mid
+            elif mid > 0 and nums[mid - 1] > nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return mid
