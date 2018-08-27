3750b7a633d05b4f416f814dd7f42875b91412e3
Kent Shih <shihm@oregonstate.edu>
2018-08-26 23:21:10 -0700

    power of two

--- poweroftwo.py
+++ poweroftwo.py
@@ -4,3 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        if n < 1:
+            return False
+        one = False
+        while n > 0:
+            if n & 1:
+                if one:
+                    return False
+                else:
+                    one = True
+            n >>= 1
+        return one
