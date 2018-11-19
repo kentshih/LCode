bf7b6abf97fe9d3a3a6dc9224ea3968047f53447
Kent Shih <shihm@oregonstate.edu>
2018-11-19 22:44:09 +0800

    symmetric tree

--- SymmetricTree.py
+++ SymmetricTree.py
@@ -5,14 +5,30 @@
 #         self.left = None
 #         self.right = None
 
+from operator import xor
+
 class Solution(object):
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        def isSame(left, right):
-            if not (left or right):
+        def isSame(left,right):
+            if not left and not right:
                 return True
-                
+            elif left != right:
+            # elif xor(left,right):
+                return False
+
+            elif left.val == right.val:
+                doleft = isSame(left.left, right.left)
+                doright= isSame(left.right, right.right)
+                return doleft and doright
+            else:
+                return False
+
+        if not root:
+            return False
+        else:
+            return isSame(root.left, root.right)
             
