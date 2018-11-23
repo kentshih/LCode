16ce123c800699a6b45ac8ec01b1453d5be31a03
Kent Shih <shihm@oregonstate.edu>
2018-11-23 23:01:47 +0800

    symetric tree

---
+++ BinTreeLevelTraversal.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        def GetHeight(root):
+            if not root:
+                return 0
+            left  = GetHeight(root.left)
+            right = GetHeight(root.right)
+            return max(left,right) + 1 
+        
+        def DFS(root, ans, height):
+            if not root:
+                return ans
+            else:
+                ans[height].append(root.val)
+                DFS(root.left, ans, height+1)
+                DFS(root.right, ans, height+1)
+                return ans 
+        
+        ans = []
+        height = GetHeight(root)
+        
+        for x in xrange(height):
+            ans.append([])
+        if height == 0:
+            return ans
+        else:
+            ans = DFS(root, ans, 0)
+            return ans
---
+++ SymmetricTree.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from operator import xor
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def isSame(left,right):
+            if not left and not right:
+                return True
+            elif left != right:
+            # elif xor(left,right):
+                return False
+
+            elif left.val == right.val:
+                doleft = isSame(left.left, right.left)
+                doright= isSame(left.right, right.right)
+                return doleft and doright
+            else:
+                return False
+
+        def BFS(root,stack):
+            if not root:
+                return stack
+            else:
+                stack.append(root.left.val)
+                stack.append(root.right.val)
+                
+                while stack:
+                    if stack[0] == stack[-1]:
+                        stack = [1:-1]
+                if stack:
+                    return []
+                else:
+                    stack = BFS(root.left,stack)
+                    stack = BFS(root.right,stack)
+
+
+        if not root:
+            return False
+        else:
+            # return isSame(root.left, root.right)
+            stack = []
+            BFS(root,stack)
+
+            
